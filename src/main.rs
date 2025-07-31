dbbd7541f004b90631184aee4e751bd0a8ab75d5
vldr <reinisvlad@gmail.com>
2025-07-30 20:56:45 -0500

    Add writer.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate fastcgi;
+
+use std::{io::Write, net::TcpListener};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:1237").unwrap();
+
+    fastcgi::run_tcp(
+        |mut req| {
+            let mut target_string = String::new();
+            use std::fmt::Write;
+
+            writeln!(target_string, "Content-Type: text/plain\n\n").unwrap();
+
+            for param in req.params() {
+                writeln!(target_string, "{:?}", param).unwrap();
+            }
+
+            write!(&mut req.stdout(), "{}", target_string).unwrap_or(());
+        },
+        &listener,
+    );
 }
