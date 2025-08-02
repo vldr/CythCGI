2496a5ba9840e2a14e3eb68c75919b86a6252196
vldr <reinisvlad@gmail.com>
2025-08-02 16:07:02 -0500

    Migrate to dashmap.

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,21 @@ use std::{
     io::Write,
     net::TcpListener,
     process::{Command, Stdio},
-    sync::Mutex,
+    sync::{Mutex, RwLock},
     time::SystemTime,
 };
 
+use dashmap::DashMap;
 use fastcgi::Request;
 use wasmtime::{
-    AsContext, AsContextMut, Caller, Config, Engine, Func, FuncType, Instance, Module, Store,
-    TypedFunc,
+    AsContext, AsContextMut, Caller, Config, Engine, Func, FuncType, Instance, Linker, Module,
+    Store, TypedFunc,
 };
 
 struct Script {
     modified: SystemTime,
-    store: Store<String>,
-    start: TypedFunc<(), ()>,
+    module: Module,
+    linker: Linker<String>,
 }
 
 const IMPORTS: &'static [u8] = b"import \"env\"
@@ -60,10 +61,8 @@ fn dedent(input: String) -> String {
     result.trim_matches('\n').to_owned()
 }
 
-fn process_request(mut req: Request, engine: &Engine, scripts: &Mutex<HashMap<String, Script>>) {
+fn process_request(mut req: Request, engine: &Engine, scripts: &DashMap<String, Script>) {
     use std::fmt::Write;
-
-    let mut scripts = scripts.lock().unwrap_or_else(|error| error.into_inner());
 
     let Some(path) = req.param("SCRIPT_FILENAME") else {
         write!(&mut req.stdout(), "Status: 500 Internal Server Error\n\n").unwrap_or(());
@@ -82,7 +81,7 @@ fn process_request(mut req: Request, engine: &Engine, scripts: &Mutex<HashMap<St
         return;
     };
 
-    let script = scripts.get_mut(&path);
+    let script = scripts.get(&path);
     if script.is_none()
         || script
             .as_ref()
@@ -90,6 +89,8 @@ fn process_request(mut req: Request, engine: &Engine, scripts: &Mutex<HashMap<St
             .modified
             .ne(&metadata.modified().unwrap())
     {
+        drop(script);
+
         let data = fs::read(&path).unwrap();
         let mut input = Vec::new();
         input.extend_from_slice(IMPORTS);
@@ -169,65 +170,74 @@ fn process_request(mut req: Request, engine: &Engine, scripts: &Mutex<HashMap<St
             return;
         }
 
-        let mut store = Store::new(&engine, String::new());
         let module = Module::from_binary(&engine, &output).unwrap();
+        let mut linker = Linker::<String>::new(&engine);
 
-        let a = module.imports().find(|a| a.name() == "print").unwrap();
+        let func_import = module.imports().find(|a| a.name() == "print").unwrap();
+        let func_ty = FuncType::new(&engine, func_import.ty().unwrap_func().params(), []);
+        linker
+            .func_new("env", "print", func_ty, |mut poop, params, _results| {
+                let a = params.get(0).unwrap().unwrap_any_ref().unwrap();
+                let a = a.as_array(poop.as_context()).unwrap().unwrap();
 
-        let func_ty = FuncType::new(&engine, a.ty().unwrap_func().params(), []);
-        let func = Func::new(&mut store, func_ty, |mut poop, params, _results| {
-            let a = params.get(0).unwrap().unwrap_any_ref().unwrap();
-            let a = a.as_array(poop.as_context()).unwrap().unwrap();
+                let mut result = String::new();
 
-            let mut result = String::new();
+                for p in a.elems(poop.as_context_mut()).unwrap() {
+                    let int_val = p.i32().unwrap();
+                    let ch = std::char::from_u32(int_val as u32).unwrap();
 
-            for p in a.elems(poop.as_context_mut()).unwrap() {
-                let int_val = p.i32().unwrap();
-                let ch = std::char::from_u32(int_val as u32).unwrap();
+                    result.push(ch);
+                }
 
-                result.push(ch);
-            }
+                write!(poop.data_mut(), "{}", result).unwrap();
 
-            write!(poop.data_mut(), "{}", result).unwrap();
+                Ok(())
+            })
+            .unwrap();
 
-            Ok(())
-        });
+        println!("Compiling and running {}", path);
 
-        let instance = Instance::new(&mut store, &module, &[func.into()]).unwrap();
+        let mut store = Store::new(&engine, String::new());
+        let instance = linker.instantiate(&mut store, &module).unwrap();
         let start = instance
             .get_typed_func::<(), ()>(&mut store, "<start>")
             .unwrap();
 
-        let mut script = Script {
-            modified: metadata.modified().unwrap(),
-            store,
-            start,
-        };
-
-        script.store.data_mut().clear();
-        script.start.call(&mut script.store, ()).unwrap();
-
-        println!("Compiling and running {}", path);
+        start.call(&mut store, ()).unwrap();
 
         write!(
             &mut req.stdout(),
             "Content-Type: text/plain\n\n{}",
-            script.store.data()
+            store.data()
         )
         .unwrap_or(());
+
+        let script = Script {
+            modified: metadata.modified().unwrap(),
+            module,
+            linker,
+        };
 
         scripts.insert(path, script);
     } else {
         println!("Caching {}", path);
 
         let script = script.unwrap();
-        script.store.data_mut().clear();
-        script.start.call(&mut script.store, ()).unwrap();
+        let mut store = Store::new(&engine, String::new());
+        let instance = script
+            .linker
+            .instantiate(&mut store, &script.module)
+            .unwrap();
+        let start = instance
+            .get_typed_func::<(), ()>(&mut store, "<start>")
+            .unwrap();
+
+        start.call(&mut store, ()).unwrap();
 
         write!(
             &mut req.stdout(),
             "Content-Type: text/plain\n\n{}",
-            script.store.data()
+            store.data()
         )
         .unwrap_or(());
     }
@@ -242,7 +252,7 @@ fn main() {
     let engine = Engine::new(&config).unwrap();
 
     let listener = TcpListener::bind("127.0.0.1:1237").unwrap();
-    let scripts = Mutex::new(HashMap::<String, Script>::new());
+    let scripts = DashMap::<String, Script>::new();
 
     fastcgi::run_tcp(
         move |req| process_request(req, &engine, &scripts),
