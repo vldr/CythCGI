acbad2d4092ebffb572e0147489de31c02d69ca3
Vlad <vlad@vldr.org>
2025-08-19 20:48:17 -0500

    Expose the GC.

--- src/main.rs
+++ src/main.rs
@@ -1319,6 +1319,7 @@ fn main() -> ExitCode {
     }
 
     let platform = v8::new_default_platform(0, false).make_shared();
+    v8::V8::set_flags_from_string("--expose-gc");
     v8::V8::initialize_platform(platform);
     v8::V8::initialize();
 
