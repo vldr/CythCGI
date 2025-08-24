a316574661f1d34a2db127882e48f75e793ed944
Vlad <vlad@vldr.org>
2025-08-23 21:25:42 -0500

    No loop.

--- vendor/fastcgi/src/lib.rs
+++ vendor/fastcgi/src/lib.rs
@@ -689,13 +689,8 @@ where
                 let handler = handler.clone();
 
                 let sock = Rc::new(sock);
-                loop {
-                    let (request_id, role, keep_conn) = Request::begin(&sock).unwrap();
-                    handler(Request::new(sock.clone(), request_id, role).unwrap());
-                    if !keep_conn {
-                        break;
-                    }
-                }
+                let (request_id, role, keep_conn) = Request::begin(&sock).unwrap();
+                handler(Request::new(sock.clone(), request_id, role).unwrap());
             }
         });
 
