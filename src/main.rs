28cbf075fceb5418327f9beecbc65bcea90574a2
Vlad <vlad@vldr.org>
2025-08-23 22:40:07 -0500

    Remove.

--- src/main.rs
+++ src/main.rs
@@ -1492,7 +1492,6 @@ fn main() -> ExitCode {
             return ExitCode::FAILURE;
         }
         config.cranelift_flag_enable("has_sse41");
-        config.cranelift_flag_enable("has_avx");
         config.cranelift_flag_enable("has_avx2");
     }
 
