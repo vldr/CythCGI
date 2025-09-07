7caf893b35fd4d3ce8580787bb7271aba9274d29
vldr <reinisvlad@gmail.com>
2025-09-06 23:11:07 -0500

    Refactor output functionality.

--- src/main.rs
+++ src/main.rs
@@ -416,6 +416,31 @@ fn val_to_char_array(caller: &mut Caller<'_, Context>, val: &Val) -> Vec<u8> {
     result
 }
 
+fn val_to_char_array_and_output(caller: &mut Caller<'_, Context>, val: &Val) {
+    let class = val.unwrap_any_ref().unwrap();
+    let class = class.unwrap_struct(caller.as_context()).unwrap();
+
+    let size = class.field(caller.as_context_mut(), 1).unwrap();
+    let size = size.unwrap_i32();
+
+    let array = class.field(caller.as_context_mut(), 0).unwrap();
+    let array = array.unwrap_any_ref().unwrap();
+    let array = array.as_array(caller.as_context()).unwrap().unwrap();
+
+    let mut output = std::mem::take(&mut caller.data_mut().output);
+    output.reserve(size as usize);
+
+    for elem in array
+        .elems(caller.as_context_mut())
+        .unwrap()
+        .take(size as usize)
+    {
+        output.push(elem.unwrap_i32() as u8 as char);
+    }
+
+    caller.data_mut().output = output;
+}
+
 fn char_array_to_val(caller: &mut Caller<'_, Context>, buf: Vec<u8>) -> Val {
     let array_ty = ArrayType::new(
         caller.engine(),
@@ -462,6 +487,22 @@ fn val_to_string(caller: &mut Caller<'_, Context>, val: &Val) -> String {
     }
 
     unsafe { String::from_utf8_unchecked(result) }
+}
+
+fn val_to_string_and_ouput(caller: &mut Caller<'_, Context>, val: &Val) {
+    let array = val.unwrap_any_ref().unwrap();
+    let array = array.as_array(caller.as_context()).unwrap().unwrap();
+
+    let len = array.len(caller.as_context()).unwrap();
+
+    let mut output = std::mem::take(&mut caller.data_mut().output);
+    output.reserve(len as usize);
+
+    for elem in array.elems(caller.as_context_mut()).unwrap() {
+        output.push(elem.unwrap_i32() as u8 as char);
+    }
+
+    caller.data_mut().output = output;
 }
 
 fn val_to_externref<'a, T: 'static>(
@@ -622,9 +663,8 @@ fn link_script(engine: &Engine, module: &Module) -> InstancePre<Context> {
                 "env",
                 "print",
                 func_ty.ty().func().unwrap().clone(),
-                |mut caller, params, _results| {
-                    let result = val_to_string(&mut caller, params.get(0).unwrap());
-                    caller.data_mut().output.push_str(&result);
+                |mut caller: Caller<'_, Context>, params, _results| {
+                    val_to_string_and_ouput(&mut caller, params.get(0).unwrap());
 
                     Ok(())
                 },
@@ -639,8 +679,8 @@ fn link_script(engine: &Engine, module: &Module) -> InstancePre<Context> {
                 "println",
                 func_ty.ty().func().unwrap().clone(),
                 |mut caller, params, _results| {
-                    let result = val_to_string(&mut caller, params.get(0).unwrap());
-                    caller.data_mut().output.push_str(&result);
+                    val_to_string_and_ouput(&mut caller, params.get(0).unwrap());
+
                     caller.data_mut().output.push('\n');
 
                     Ok(())
@@ -656,9 +696,7 @@ fn link_script(engine: &Engine, module: &Module) -> InstancePre<Context> {
                 "printBuffer",
                 func_ty.ty().func().unwrap().clone(),
                 |mut caller, params, _results| {
-                    let result = val_to_char_array(&mut caller, params.get(0).unwrap());
-                    let result = unsafe { String::from_utf8_unchecked(result) };
-                    caller.data_mut().output.push_str(&result);
+                    val_to_char_array_and_output(&mut caller, params.get(0).unwrap());
 
                     Ok(())
                 },
